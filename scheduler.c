4f8972563c97f6dcad5d0824deb5b29a75a54f25
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-11-28 19:34:27 -0300

    Implement 'enqueue_by_class' and 'look_next_queue_to_be_called'

--- scheduler.c
+++ scheduler.c
@@ -14,3 +14,36 @@ void initialize_scheduler(Scheduler* scheduler, int cashier_count, int time_per_
     scheduler->call_order[i] = call_order[i];
   }
 }
+
+int enqueue_by_class(Scheduler* scheduler, int class_num, int account, int oper_count) {
+  Queue **queue = &scheduler->queues[class_num - 1];
+
+  enqueue(queue, account, oper_count);
+}
+
+int look_next_queue_to_be_called(Scheduler* scheduler) {
+  int threshold_01 = scheduler->call_order[0]; 
+  int threshold_02 = scheduler->call_order[1] + threshold_01;
+  int threshold_03 = scheduler->call_order[2] + threshold_02;
+  int threshold_04 = scheduler->call_order[3] + threshold_03; 
+  int threshold_05 = scheduler->call_order[4] + threshold_04;
+
+  int call_order = scheduler->iteration % threshold_05; 
+
+  // Queue 01;
+  if (call_order < threshold_01) {
+    return 1;
+  // Queue 02;
+  } else if (call_order < threshold_02) {
+    return 2;
+  // Queue 03;
+  } else if (call_order < threshold_03) {
+    return 3;
+  // Queue 04;
+  } else if (call_order < threshold_04) {
+    return 4;
+  // Queue 05;
+  } else {
+    return 5;
+  }
+}
