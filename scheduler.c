b1ca0ae3dc7dea0224de505f6d58287b1ec7400b
Gustavo Costa <74384665+Lu1z-Gust4v0@users.noreply.github.com>
2022-12-01 12:21:56 +0000

    Remove memory allocation for scheduler in 'initialize_scheduler'

--- scheduler.c
+++ scheduler.c
@@ -2,15 +2,14 @@
 
 void initialize_scheduler(Scheduler* scheduler, int cashier_count, int time_per_oper, int call_order[5]) {
   int i;
-  // Allocate memory for the scheduler.
-  scheduler = (Scheduler*) malloc(sizeof(Scheduler));
+
   scheduler->cashier_count = cashier_count;
   scheduler->time_per_operation = time_per_oper;
   scheduler->iteration = 0;
 
   for (i = 0; i < 5; i++) {
     // Allocate memory for each queue.
-    scheduler->queues[i] = (Queue*) malloc(sizeof(Queue));
+    initialize_queue(&scheduler->queues[i]);
     scheduler->call_order[i] = call_order[i];
   }
 }
