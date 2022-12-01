c79d78775b5357ba3cc3f6a65d06388bab1e2ff5
Gustavo Costa <74384665+Lu1z-Gust4v0@users.noreply.github.com>
2022-12-01 13:58:44 +0000

    Define and implement functions that get the info from the config file

---
+++ utils.c
@@ -0,0 +1,67 @@
+#include "utils.h"
+
+int obter_qntd_caixas(char* buffer) {
+	char* token;
+
+	strtok(buffer, "=");
+	token = strtok(NULL, "=");
+
+	return atoi(token);
+}
+
+int obter_tempo_por_oper(char* buffer) {
+	char* token;
+
+	strtok(buffer, "=");
+	token = strtok(NULL, "=");
+
+	return atoi(token);
+}
+
+int* obter_disc_escalonamento(char* buffer) {
+	int i;
+	int* tmp;
+	char* token;
+
+	tmp = (int*) malloc(sizeof(int) * 5);
+
+	strtok(buffer, "=");
+	strtok(NULL, "{");
+	token = strtok(NULL, "}");
+
+	i = 0;
+	token = strtok(token, ",");
+	while (token != NULL) {
+		tmp[i] = atoi(token);
+		token = strtok(NULL, ",");
+		i++;
+	}
+
+	return tmp;
+}
+
+Cliente* obter_info_cliente(char* buffer) {
+	Cliente *cliente;
+	char* token_01;
+	char* token_02;
+	char* token_03;
+
+	cliente = (Cliente*) malloc(sizeof(Cliente));
+	token_01 = strtok(buffer, "-");
+	token_02 = strtok(NULL, "-");
+	token_03 = strtok(NULL, "-");
+
+	cliente->classe = (char*) malloc(sizeof(char) * (strlen(token_01) + 1));
+	strcpy(cliente->classe, token_01);
+
+	strtok(token_02, " ");
+	cliente->num_conta = atoi(strtok(NULL, " "));
+
+	cliente->qntd_opereracoes = atoi(strtok(token_03, " "));
+
+	printf("classe: %s\n", cliente->classe);
+	printf("num conta: %i\n", cliente->num_conta);
+	printf("qntd opertacoes: %i\n", cliente->qntd_opereracoes);
+
+	return cliente;
+}
---
+++ utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char* classe;
+	int num_conta;
+	int qntd_opereracoes;
+} Cliente;
+
+int obter_qntd_caixas(char* buffer); 
+int obter_tempo_por_oper(char* buffer);
+int* obter_disc_escalonamento(char* buffer);
+Cliente* obter_info_cliente(char* buffer);
+
+#endif /* UTILS_H */
