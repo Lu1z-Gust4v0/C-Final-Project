d9b0374eafce0679e20b09ff7d49cd0d4f0e7ce1
Gustavo Costa <74384665+Lu1z-Gust4v0@users.noreply.github.com>
2022-12-06 15:00:12 +0000

    Define logtree functions

---
+++ logtree.h
@@ -0,0 +1,29 @@
+#ifndef LOG_TREE_H
+#define LOG_TREE_H
+
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct tree_node {
+	struct tree_node* pai; 
+	struct tree_node* direita; 
+	struct tree_node* esquerda; 
+	int conta; 
+	int classe;
+	int caixa; 
+	int timer;
+	int operacoes;
+} TREE_NODE;
+
+typedef struct {
+	TREE_NODE *raiz;
+} Log;
+
+void log_inicializar(Log **l);
+void log_registrar(Log **l, int conta, int classe, int timer, int caixa, int ops);
+float log_media_por_classe(Log **l, int classe);
+float log_media_ops_por_classe(Log **l, int classe);
+int log_obter_soma_por_classe(Log **l, int classe);
+int log_obter_contagem_por_classe(Log **l, int classe);
+
+#endif /* LOG_TREE_H */
