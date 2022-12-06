e9f9578155ffde11af539861da5b79e5f08af836
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-12-06 00:09:57 -0300

    Fix 'consultar_prox_fila' logic error, and code most of 'e_rodar'

--- escalonador.c
+++ escalonador.c
@@ -3,9 +3,10 @@
 void e_inicializar(Escalonador *e, int caixas, int delta_t, int n_1, int n_2, int n_3, int n_4, int n_5) {
   int i;
 
-  e->caixas = caixas;
+  e->qntd_caixas = caixas;
   e->tempo_por_operacao = delta_t;
   e->iteracao = 0;
+  e->fila_atual = 0;
 
   e->ordem_de_chamada[0] = n_1;
   e->ordem_de_chamada[1] = n_2; 
@@ -13,9 +14,20 @@ void e_inicializar(Escalonador *e, int caixas, int delta_t, int n_1, int n_2, in
   e->ordem_de_chamada[3] = n_4;
   e->ordem_de_chamada[4] = n_5;
 
+  e->caixas = (Caixa*) malloc(sizeof(Caixa) * caixas);
+  if (e->caixas == NULL) {
+    printf("[Error] Failed to allocate memory for 'cashiers'.\n");
+    return;
+  }
+
+  // Allocate memory for each queue.
   for (i = 0; i < 5; i++) {
-    // Allocate memory for each queue.
     f_inicializar(&e->filas[i]);
+  }
+
+  for (i = 0; i < caixas; i++) {
+    e->caixas[i].atendimentos = 0;
+    e->caixas[i].tempo_de_espera = 0;
   }
 }
 
@@ -24,33 +36,16 @@ int e_inserir_por_fila(Escalonador *e, int classe, int num_conta, int qtde_opera
 }
 
 int e_consultar_prox_fila(Escalonador *e) {
-  int threshold_01 = e->ordem_de_chamada[0]; 
-  int threshold_02 = e->ordem_de_chamada[1] + threshold_01;
-  int threshold_03 = e->ordem_de_chamada[2] + threshold_02;
-  int threshold_04 = e->ordem_de_chamada[3] + threshold_03; 
-  int threshold_05 = e->ordem_de_chamada[4] + threshold_04;
-  int ordem_de_chamada = e->iteracao % threshold_05; 
-  int fila_num;
+  int fila_num = e->fila_atual;
+  int counter = 0;
 
-  // Queue 01;
-  if (ordem_de_chamada < threshold_01) {
-    fila_num = 0;
-  // Queue 02;
-  } else if (ordem_de_chamada < threshold_02) {
-    fila_num = 1;
-  // Queue 03;
-  } else if (ordem_de_chamada < threshold_03) {
-    fila_num = 2;
-  // Queue 04;
-  } else if (ordem_de_chamada < threshold_04) {
-    fila_num = 3;
-  // Queue 05;
-  } else {
-    fila_num = 4;
-  }
-
-  while(f_num_elementos(&e->filas[fila_num]) == 0) {
-    fila_num = (fila_num + 1) % 5;
+  while(e->iteracao == e->ordem_de_chamada[fila_num] || f_num_elementos(&e->filas[fila_num]) == 0) {
+    counter++;
+    // Edge case: All remaining elements are from a single class;
+    if (counter == 5) {
+      e->iteracao = 0;
+    }
+    fila_num = (fila_num + 1) % 5;  
   }
 
   return fila_num;
@@ -73,7 +68,8 @@ int e_consultar_prox_qtde_oper(Escalonador *e) {
 }
 
 int e_consultar_qtde_clientes(Escalonador *e) {
-  int i; int qntd = 0;
+  int i; 
+  int qntd = 0;
 
   for (i = 0; i < 5; i++) {
     qntd += f_num_elementos(&e->filas[i]);
@@ -91,14 +87,20 @@ int e_consultar_tempo_prox_cliente(Escalonador *e) {
 }
 
 int e_obter_prox_num_conta(Escalonador *e) {
-  int conta, num_fila;
+  int conta, fila_anterior;
 
-  num_fila = e_consultar_prox_fila(e);
-  conta = f_obter_proxima_chave(&e->filas[num_fila]);
+  fila_anterior = e->fila_atual;
+  
+  e->fila_atual = e_consultar_prox_fila(e);
+  if (fila_anterior != e->fila_atual) {
+    e->iteracao = 0;
+  }
 
+  conta = f_obter_proxima_chave(&e->filas[e->fila_atual]);
   // Increment scheduler iteration
-  e->iteracao++;
-
+  if (conta != -1) {
+    e->iteracao++;
+  }
   return conta;
 }
 
@@ -131,7 +133,66 @@ int e_conf_por_arquivo(Escalonador *e, char *nome_arq_conf) {
       free_client_ptr(cliente);
     }
 	}
+
 	fclose(file);
   
   return 1;
 }
+
+int chamar_cliente(Escalonador *e, FILE* file, int num_caixa, int tempo) {
+  int classe_num, num_conta, num_operacoes, tempo_de_espera;
+  char *classe;
+
+  tempo_de_espera = e->caixas[num_caixa].tempo_de_espera - e->tempo_por_operacao; 
+  // The cashier is free.
+  if (e->caixas[num_caixa].atendimentos == 0 || tempo_de_espera == 0) {
+    classe_num = e_consultar_prox_fila(e) + 1;
+    classe = obter_classe(classe_num);
+    num_operacoes = e_consultar_prox_qtde_oper(e);
+    num_conta = e_consultar_prox_num_conta(e);
+
+    fprintf(
+      file, 
+      "T = %i min: Caixa %i chama da categoria %s cliente da conta %i para realizar %i operacao(oes).\n", 
+      tempo, (num_caixa + 1), classe, num_conta, num_operacoes
+    );
+
+    e->caixas[num_caixa].tempo_de_espera = num_operacoes * e->tempo_por_operacao;
+    e->caixas[num_caixa].atendimentos++;
+
+    free(classe);
+    return 1;
+  } else {
+    e->caixas[num_caixa].tempo_de_espera -= e->tempo_por_operacao;
+    return 0;
+  }
+}
+
+void e_rodar(Escalonador *e, char *nome_arq_in, char *nome_arq_out) {
+  FILE* file;
+  int qntd_clientes, i, tempo = 0;
+
+  if (e_conf_por_arquivo(e, nome_arq_in) == 0) {
+    printf("[Error] Failed to configure.\n");
+    return;
+  };
+
+  file = fopen(nome_arq_out, "w");
+	if (file == NULL) {
+		printf("[Error] Could not open file '%s'.\n", nome_arq_out);
+		return;
+	}
+
+  qntd_clientes = e_consultar_qtde_clientes(e);
+
+  while (qntd_clientes != 0) {
+    for (i = 0; i < e->qntd_caixas; i++) {
+      if(qntd_clientes != 0 && chamar_cliente(e, file, i, tempo)) {
+        e_obter_prox_num_conta(e);
+        qntd_clientes--;
+      };
+    }
+    tempo += e->tempo_por_operacao; 
+  }
+  fclose(file);
+} 
